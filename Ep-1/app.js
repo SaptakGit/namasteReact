7110c5ae68d4aa0b6653af4e9f1249f66eed8e60
SaptakGit <sadcat14@gmail.com>
2024-11-30 16:00:15 +0530

    Episode 1

---
+++ Ep-1/app.js
@@ -0,0 +1,25 @@
+const heading = React.createElement(
+    "h1", // Tag name
+    {id:"heading", name:"react_heading"},  // attributes
+    "Hello World from React" // children/text/what we want to show. This can be one children or array of multiple children
+);
+
+const parent = React.createElement("div", {id:"parent"},
+    [   React.createElement("div", {id:"child"},
+            [React.createElement("h1", {},"I'm H1 Tag"),
+            React.createElement("h2", {},"I'm H2 Tag")]
+        ),
+        React.createElement("div", {id:"child2"},
+            [React.createElement("h1", {},"I'm H1 Tag"),
+            React.createElement("h2", {},"I'm H2 Tag")]
+        )
+    ]
+);
+
+console.log(heading) // is an object
+console.log(parent) 
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+root.render(parent);
